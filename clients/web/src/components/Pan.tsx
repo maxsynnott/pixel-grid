22e6966bd6e1cd6e24a5bc2916524e7f569db276
Max Synnott <maxryansynnott@gmail.com>
2021-12-14 13:19:26 +1300

    Made pan speed dependent on zoom

--- clients/web/src/components/Pan.tsx
+++ clients/web/src/components/Pan.tsx
@@ -1,31 +1,34 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useContext, useEffect, useState } from "react";
+import { ZoomContext } from "../contexts/ZoomContext";
 
-const INCREMENT = 15;
+const BASE_INCREMENT = 100;
 
 export const Pan: FC = ({ children }) => {
+	const zoom = useContext(ZoomContext);
 	const [x, setX] = useState(0);
 	const [y, setY] = useState(0);
 
+	const increment = BASE_INCREMENT / zoom;
 	useEffect(() => {
 		const handleKeyDown = (event: KeyboardEvent) => {
 			switch (event.key) {
 				case "ArrowUp":
-					setY(y + INCREMENT);
+					setY(y + increment);
 					break;
 				case "ArrowRight":
-					setX(x - INCREMENT);
+					setX(x - increment);
 					break;
 				case "ArrowDown":
-					setY(y - INCREMENT);
+					setY(y - increment);
 					break;
 				case "ArrowLeft":
-					setX(x + INCREMENT);
+					setX(x + increment);
 					break;
 			}
 		};
 		window.addEventListener("keydown", handleKeyDown);
 		return () => window.removeEventListener("keydown", handleKeyDown);
-	}, [x, y]);
+	}, [x, y, increment]);
 
 	return (
 		<div style={{ transform: `translate(${x}px, ${y}px)` }}>{children}</div>
