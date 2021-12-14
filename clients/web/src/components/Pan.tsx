7c7adb36ceb1578441730b4ad118087fa47aaada
Max Synnott <maxryansynnott@gmail.com>
2021-12-14 20:54:32 +1300

    Store x,y in queryParams

--- clients/web/src/components/Pan.tsx
+++ clients/web/src/components/Pan.tsx
@@ -1,36 +1,47 @@
+import clamp from "just-clamp";
 import { FC, useContext, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { config } from "../config/config";
 import { ZoomContext } from "../contexts/ZoomContext";
 
 const BASE_INCREMENT = 100;
+const { width, height } = config.grid;
 
 export const Pan: FC = ({ children }) => {
-	const zoom = useContext(ZoomContext);
-	const [x, setX] = useState(0);
-	const [y, setY] = useState(0);
+	const [searchParams, setSearchParams] = useSearchParams();
 
-	const increment = BASE_INCREMENT / zoom;
+	const [x, setX] = useState(Number(searchParams.get("x") ?? width / 2));
+	const [y, setY] = useState(Number(searchParams.get("y") ?? height / 2));
+
+	const zoom = useContext(ZoomContext);
+
+	const handleKeyDown = ({ key }: KeyboardEvent) => {
+		let newX = x;
+		let newY = y;
+
+		const increment = BASE_INCREMENT / zoom;
+		if (key === "ArrowUp") newY -= increment;
+		if (key === "ArrowRight") newX += increment;
+		if (key === "ArrowDown") newY += increment;
+		if (key === "ArrowLeft") newX -= increment;
+
+		newX = clamp(0, newX, width);
+		newY = clamp(0, newY, height);
+		setX(newX);
+		setY(newY);
+		const queryParams = Object.fromEntries(searchParams.entries());
+		setSearchParams({
+			...queryParams,
+			x: newX.toFixed(2),
+			y: newY.toFixed(2),
+		});
+	};
+
 	useEffect(() => {
-		const handleKeyDown = (event: KeyboardEvent) => {
-			switch (event.key) {
-				case "ArrowUp":
-					setY(y + increment);
-					break;
-				case "ArrowRight":
-					setX(x - increment);
-					break;
-				case "ArrowDown":
-					setY(y - increment);
-					break;
-				case "ArrowLeft":
-					setX(x + increment);
-					break;
-			}
-		};
 		window.addEventListener("keydown", handleKeyDown);
 		return () => window.removeEventListener("keydown", handleKeyDown);
-	}, [x, y, increment]);
+	}, [x, y, searchParams]);
 
-	return (
-		<div style={{ transform: `translate(${x}px, ${y}px)` }}>{children}</div>
-	);
+	const transform = `translate(${width / 2 - x}px, ${height / 2 - y}px)`;
+	return <div style={{ transform }}>{children}</div>;
 };
