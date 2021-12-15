14d29ff2fcdb6fa18c95b36b52f758a62aba46a1
Max Synnott <maxryansynnott@gmail.com>
2021-12-15 16:46:24 +1300

    Added node server ecr repository

---
+++ terraform/ecr.tf
@@ -0,0 +1,27 @@
+resource "aws_ecr_repository" "node_server" {
+  name = var.node_server_ecr_repository_name
+}
+
+resource "aws_ecr_lifecycle_policy" "node_server" {
+  repository = aws_ecr_repository.node_server.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Only keep one untagged image",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "domain_name" {
   type    = string
   default = "pixelgrid.xyz"
 }
+
+variable "ecs_cluster_name" {
+  type    = string
+  default = "pixelgrid-cluster"
+}
+
+variable "node_server_ecr_repository_name" {
+  type    = string
+  default = "node-server"
+}
