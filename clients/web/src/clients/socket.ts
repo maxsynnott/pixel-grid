dabc2294bc0b4d62d0645b4c4054088b80b81f0b
Max Synnott <maxryansynnott@gmail.com>
2021-12-16 15:57:15 +1300

    Configured client websockets to point at prod server

--- clients/web/src/clients/socket.ts
+++ clients/web/src/clients/socket.ts
@@ -1,7 +1,4 @@
 import { io } from "socket.io-client";
+import { config } from "../config/config";
 
-export const socket = io("http://localhost:8080");
-
-// socket.on("connect", () => {
-// 	console.log("Connected", { id: socket.id });
-// });
+export const socket = io(config.api.baseUrl);
